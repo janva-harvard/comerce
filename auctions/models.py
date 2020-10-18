92d5b5632d7fc1de58e3bb4baede0dd02f2d8e51
Janne Väisänen <janva415@student.liu.se>
2020-10-18 17:10:04 +0300

    A initial models and first view

---
+++ auctions/models.py
@@ -0,0 +1,47 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    pass
+
+#  listing1 = Auction_listing(title="item", description="some item desceription",
+#   starting_bid=10.00, image_url="http://test.com", active=True, owner= user1)
+# user1 = User()
+
+
+class AuctionListing(models.Model):
+    """
+    docstring
+    """
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    starting_bid = models.DecimalField(max_digits=5, decimal_places=2)
+    image_url = models.URLField(max_length=200, blank=True, default=None)
+    active = models.BooleanField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Bid (models.Model):
+    amount = models.DecimalField(max_digits=5, decimal_places=2)
+    for_listing = models.ForeignKey(AuctionListing, related_name="bids",
+                                    on_delete=models.CASCADE)
+    # related_name="bids")
+    bidder = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="bidders")
+
+
+class WatchList (models.Model):
+    watcher = models.ForeignKey(User, on_delete=models.CASCADE)
+    listing = models.ForeignKey(AuctionListing, on_delete=models.CASCADE)
+
+# class Comment(models.Model):
+#     """
+#     docstring
+#     """
+#     pass
+
+
+# class Category (models.Model):
+#     pass
+#
