20dbcbec7a0332b277711525dd53bed2ab19676b
Janne Väisänen <janva415@student.liu.se>
2020-11-24 20:06:35 +0200

    Add style to add new listing

--- auctions/forms.py
+++ auctions/forms.py
@@ -4,24 +4,44 @@ from .models import Category
 
 class NewListingsForm (forms.Form):
     title = forms.CharField(label='Title',
+                            widget=forms.TextInput(attrs={
+                                'class': 'form-control '}
+                            ),
                             max_length=20,
                             required=True)
     description = forms.CharField(label='Description',
-                                  widget=forms.Textarea,
+                                  #   widget=forms.TextInput,
+                                  widget=forms.Textarea(
+                                      attrs={'class': 'form-control '}),
                                   max_length=200,
                                   required=True)
     starting_bid = forms.DecimalField(label="Starting bid",
+                                      widget=forms.NumberInput(
+                                          attrs={
+                                              'class': 'form-control  '
+                                          }),
                                       max_digits=5,
                                       decimal_places=2,
                                       required=True)
-    image_url = forms.URLField(label='Image URL', required=False)
+    image_url = forms.URLField(label='Image URL',
+                               widget=forms.TextInput(attrs={
+                                     'class': 'form-control'}),
+                               required=False)
 
     active = forms.BooleanField(label="Activate",
+                                widget=forms.CheckboxInput(
+                                    attrs={
+                                        'class': 'form-check'
+                                    }
+                                ),
                                 required=False,
                                 initial=False,
                                 )
     # Hmm I'm not sure i like this
     category = forms.ModelChoiceField(
+        widget=forms.Select(attrs={
+            'class': 'form-control'
+        }),
         queryset=Category.objects.all(),
         required=False,
         blank=True)
